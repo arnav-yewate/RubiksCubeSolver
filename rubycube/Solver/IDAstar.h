2b6d65626498c584d027242bddb7ccf9c4b8e77a
NOVA <arnavyewate@gmail.com>
2026-05-21 15:45:43 +0530

    changed the implementation of the ida star

--- rubycube/Solver/IDAstar.h
+++ rubycube/Solver/IDAstar.h
@@ -2,129 +2,101 @@
 #ifndef IDAstarSolver_H
 #define IDAstarSolver_H
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include "../Model/RCube.h"
 #include "../PatternDatabases/CornerPattern.h"
 
-//t type and h hash function
-template<typename T,typename H>
+// t type and h hash function
+template <typename T, typename H>
 class IDAstarSolver
 {
 private:
     CornerPattern cornerDB;
     vector<RubiksCube::MOVE> moves;
-    unordered_map<T, RubiksCube::MOVE, H> move_done;
-    unordered_map<T, bool, H> vis;
 
-    struct Node 
+    int dfs(T current_cube, int depth, int bound)
     {
-        T cube;
-        int depth;
-        int estimate;
-        // 1step faster than  2 step assignment
-        Node(T _cube, int _depth, int _estimate) : cube(_cube), depth(_depth), estimate(_estimate) {};
-    };
+        int heuristic = cornerDB.getNumMoves(current_cube);
+        int f = depth + heuristic; // Actual distance + estimated distance
 
-    struct comparecube 
-    {
-        // cuz needed min heap bigger are thrown to the left and smaller to the right , rightmost is the top
-        bool operator()(pair<Node, int> const &p1, pair<Node, int> const &p2) 
+        // Base Case 1: We exceeded our bound limit. Prune this branch!
+        if (f > bound)
+            return f;
+
+        // Base Case 2: The cube is solved!
+        if (current_cube.isSolved())
+            return -1;
+
+        int min_next_bound = 1e9; // Infinity
+
+        // Explore all 18 moves
+        for (int i = 0; i < 18; i++)
         {
-            auto n1 = p1.first, n2 = p2.first;
-            if (n1.depth + n1.estimate == n2.depth + n2.estimate) 
-            {
-                return n1.estimate > n2.estimate;
-            } 
-            else return n1.depth + n1.estimate > n2.depth + n2.estimate;
-        }
-    };
+            auto curr_move = static_cast<RubiksCube::MOVE>(i);
 
-    void resetstruct() 
-    {
-        moves.clear();
-        move_done.clear();
-        vis.clear();
+            if (!moves.empty())
+            {
+                auto last_move = moves.back();
+
+                // If the current move is on the exact same face as the last move, skip it!
+                if (static_cast<int>(curr_move) / 3 == static_cast<int>(last_move) / 3)
+                {
+                    continue;
+                }
+            }
+
+            // 1. Make the move and add to stack
+            current_cube.move(curr_move);
+            moves.push_back(curr_move);
+
+            // 2. Recurse deeper!
+            int t = dfs(current_cube, depth + 1, bound);
+
+            if (t == -1)
+                return -1;
+            if (t < min_next_bound)
+                min_next_bound = t;
+
+            // 3. Backtrack (Undo the move to explore the next branch)
+            moves.pop_back();
+            current_cube.invert(curr_move);
+        }
+
+        return min_next_bound; // Tell the main loop how deep to go next time
     }
 
     // returns {solved cube, bound}: if the cube was solved
     // returns {rubiksCube, next_bound}, if the cube was not solved
-    pair<T, int> IDAstar(int bound) 
-    {
-        //pair(Node, move  to reach )
-        priority_queue<pair<Node, int>, vector<pair<Node, int>>, comparecube> pq;
-        Node start = Node(cube, 0, cornerDB.getNumMoves(cube));
-        pq.push(make_pair(start, 0));
-        int next_bound = 100;
-        while (!pq.empty()) 
-        {
-            auto p = pq.top();
-            Node node = p.first;
-            pq.pop();
-
-            if (vis[node.cube]) continue;
-
-            vis[node.cube] = true;
-            move_done[node.cube] = RubiksCube::MOVE(p.second);
-
-            if (node.cube.isSolved()) return make_pair(node.cube, bound);
-            node.depth++;
-            for (int i = 0; i < 18; i++) 
-            {
-                auto curr_move = RubiksCube::MOVE(i);
-                node.cube.move(curr_move);
-                if (!vis[node.cube]) 
-                {
-                    node.estimate = cornerDB.getNumMoves(node.cube);
-                    if (node.estimate + node.depth > bound) 
-                    {
-                        next_bound = min(next_bound, node.estimate + node.depth);
-                    }
-                    else
-                    {
-                        pq.push(make_pair(node, i));
-                    }
-                }
-                node.cube.invert(curr_move);
-            }
-
-        }
-        return make_pair(cube, next_bound);
-    }
 
 public:
     T cube;
 
-    IDAstarSolver(T rcube  , string fileName)
+    IDAstarSolver(T rcube, string fileName)
     {
         cube = rcube;
         cornerDB.fromFile(fileName);
     }
 
-    vector<RubiksCube::MOVE> solve() 
+    vector<RubiksCube::MOVE> solve()
     {
-        int bound = 1;
-        auto p = IDAstar(bound);
-        while (p.second != bound) 
+        int bound = cornerDB.getNumMoves(cube); // Initial heuristic
+        moves.clear();                          // This vector will act as our stack
+
+        while (true)
         {
-            resetstruct();
-            bound = p.second;
-            p = IDAstar(bound);
+            // Call the recursive DFS search
+            int next_bound = dfs(cube, 0, bound);
+
+            // If search returns -1, it means we found the solution!
+            if (next_bound == -1)
+            {
+                return moves;
+            }
+
+            // Otherwise, update the bound and search deeper
+            bound = next_bound;
         }
-        T solved_cube = p.first;
-        assert(solved_cube.isSolved());
-        T curr_cube = solved_cube;
-        while (!(curr_cube == cube)) 
-        {
-            RubiksCube::MOVE curr_move = move_done[curr_cube];
-            moves.push_back(curr_move);
-            curr_cube.invert(curr_move);
-        }
-        cube = solved_cube;
-        reverse(moves.begin(), moves.end());
-        return moves;
     }
 };
 
-
-    #endif 
+#endif
--- rubycube/main.cpp
+++ rubycube/main.cpp
@@ -13,21 +13,10 @@ int main()
 {
     Cube1D dd;
 
-    vector<RubiksCube::MOVE> g = dd.randomshuffle(6),h;
-    reverse(g.begin(),g.end());
-
-    IDDFSSolver<Cube1D, Cube1D:: Hash1d> bfs(dd,6);
-    h = bfs.solve();
-
-    
+    dd.move("U");
+     dd.move("D");
     dd.print();
-    cout<<h.size()<<endl;
-    for(int i=0;i<h.size();i++)
-    {
-        dd.move(h[i]);
-    }
-    dd.print();
-
+   
 
 
 
