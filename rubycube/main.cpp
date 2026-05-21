b27335baf179b2ba7afef6ab7cce11bbc9db3640
NOVA <arnavyewate@gmail.com>
2026-05-21 19:58:03 +0530

    dik

--- rubycube/main.cpp
+++ rubycube/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+#include <chrono>
 #include "Model/Cube1D.cpp"
 #include "Model/Cube3D.cpp"
 #include "Solver/DFSSolver.h"
@@ -12,20 +12,28 @@ using namespace std;
 
 int main()
 {
-    Cube3D dd;
-    dd.randomshuffle(15);
-    IDAstarSolver<Cube3D,Cube3D::Hash3d> g(dd,"corners.bin");
+    Cube1D dd;
+    dd.randomshuffle(12);
+    IDAstarSolver<Cube1D,Cube1D::Hash1d> g(dd,"corners.bin");
+    auto start_time = chrono::high_resolution_clock::now();
     vector<RubiksCube::MOVE> h= g.solve();
+
+    auto end_time = chrono::high_resolution_clock::now();
     dd.print();
     for(int i=0;i<h.size();i++)
     {
         cout<<RubiksCube::getMove(h[i])<<endl;
         dd.move(h[i]);
     }
-    
+    cout<<endl;
     dd.print();
     
+    auto duration_ms = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+    double duration_seconds = duration_ms / 1000.0;
 
+    // Print the results
+    cout << "\nSolution Found! Total Moves: " << h.size() << endl;
+    cout << "Time Taken: " << duration_ms << " ms (" << duration_seconds << " seconds)" << endl;
     
 
 
